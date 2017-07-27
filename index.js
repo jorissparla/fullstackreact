9ad71ee304f4fab683dd2998e134e3fe90e8fb50
Joris Sparla <joris.sparla@infor.com>
2017-07-27 17:13:31 +0200

    auth

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 3600 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-passport.use(new GoogleStrategy({}));
+require('./routes/authroutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
---
+++ services/passport.js
@@ -0,0 +1,40 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken);
+      console.log('😝 refreshToken', refreshToken);
+      console.log('😎 profile', profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id, googleName: profile.displayName })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  )
+);
